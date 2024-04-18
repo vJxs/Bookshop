6d55fe7eb9b98394984549ebc760d9dd44542cc7
vJxs <Jelaniblenman@gmail.com>
2024-04-18 15:56:24 -0400

    TailWind Css (FrontEnd)

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+ 
+
+  return (
+    <>
+     <h1 className='text-teal-300 bg-violet-100 text-center text-6xl'>Welcome to the BookShop</h1>
+    </>
+  )
+}
+
+export default App
